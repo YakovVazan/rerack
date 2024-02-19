4c698262afdafccee64b7343ed99a3e3cd76a3e2
YakovVazan <yakovvazan@gmail.com>
2024-02-19 15:00:57 +0200

    refactor: udpate server URL

--- src/config/constants.js
+++ src/config/constants.js
@@ -1,5 +1,5 @@
 export const consts = {
-  baseURL: "http://localhost:3000",
+  baseURL: "https://api-rerack.onrender.com",
   typeDropDownInitialValue: "type",
   companyDropDownInitialValue: "company",
 };
