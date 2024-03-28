2642dbfbf18c518146ac8903e4fe8b6412c9708d
YakovVazan <yakovvazan@gmail.com>
2024-03-28 10:55:41 +0200

    feat: changed color of check SVG

--- src/components/svg/SvgCheck/SvgCheck.jsx
+++ src/components/svg/SvgCheck/SvgCheck.jsx
@@ -4,7 +4,7 @@ const SvgCheck = () => {
       xmlns="http://www.w3.org/2000/svg"
       width="16"
       height="16"
-      fill="currentColor"
+      fill="#6c757d"
       className="bi bi-check-lg"
       viewBox="0 0 16 16"
     >
